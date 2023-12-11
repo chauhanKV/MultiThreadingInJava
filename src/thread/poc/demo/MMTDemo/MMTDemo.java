c55dbd76581b0aa17f4148c8d951d16118048067
kavitachauhan <chauhan.kavita@hotmail.com>
2023-12-11 19:13:07 +0530

    MultiThreading - Code Refactoring

--- src/thread/poc/demo/MMTDemo/MMTDemo.java
+++ src/thread/poc/demo/MMTDemo/MMTDemo.java
@@ -1,10 +1,14 @@
 package thread.poc.demo.MMTDemo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MMTDemo {
     public static void main(String[] args) {
-        MMTFlightSearchService service = new MMTFlightSearchService();
+        List<FlightSearchInterface> flightSearchImplementationList = new ArrayList<>();
+        flightSearchImplementationList.add(new GoFlightSearchService());
+        flightSearchImplementationList.add(new IndigoFlightSearchService());
+        MMTFlightSearchService service = new MMTFlightSearchService(flightSearchImplementationList);
         System.out.println(service.getAllFlightsBySourceAndDestination("Delhi", "Norway"));
     }
 }
--- src/thread/poc/demo/MMTDemo/MMTFlightSearchService.java
+++ src/thread/poc/demo/MMTDemo/MMTFlightSearchService.java
@@ -6,32 +6,63 @@ import java.util.List;
 import java.util.concurrent.*;
 
 public class MMTFlightSearchService {
+
+    List<FlightSearchInterface> flightSearchImplementation;
+
+    public MMTFlightSearchService(List<FlightSearchInterface> flightSearchImplementation) {
+        this.flightSearchImplementation = flightSearchImplementation;
+    }
     private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public List<FlightData> getAllFlightsBySourceAndDestination(String source, String destination) {
         List<FlightData> response = new ArrayList<>();
-        GoFlightSearchService go = new GoFlightSearchService();
-        IndigoFlightSearchService indigo = new IndigoFlightSearchService();
 
-//        response.addAll(go.getFlightSearchData(source, destination));
-//        response.addAll(indigo.getFlightSearchData(source, destination));
+//        GoFlightSearchService go = new GoFlightSearchService();
+//        IndigoFlightSearchService indigo = new IndigoFlightSearchService();
+//
+////        response.addAll(go.getFlightSearchData(source, destination));
+////        response.addAll(indigo.getFlightSearchData(source, destination));
+//
+//        Callable<List<FlightData>> indigoCallable = () -> indigo.getFlightSearchData(source, destination);
+//        Callable<List<FlightData>> goCallable = () -> go.getFlightSearchData(source, destination);
+//
+//        Future<List<FlightData>> indigoFuture = executorService.submit(indigoCallable);
+//        Future<List<FlightData>> goFuture = executorService.submit(goCallable);
+//
+//
+//        try {
+//            List<FlightData> indigoResponse = indigoFuture.get();
+//            List<FlightData> goResponse = goFuture.get();
+//
+//            response.addAll(indigoResponse);
+//            response.addAll(goResponse);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        } catch (ExecutionException e) {
+//            throw new RuntimeException(e);
+//        }
 
-        Callable<List<FlightData>> indigoCallable = () -> indigo.getFlightSearchData(source, destination);
-        Callable<List<FlightData>> goCallable = () -> go.getFlightSearchData(source, destination);
+        // Code Refactoring
 
-        Future<List<FlightData>> indigoFuture = executorService.submit(indigoCallable);
-        Future<List<FlightData>> goFuture = executorService.submit(goCallable);
+        List<Callable<List<FlightData>>> callableList = new ArrayList<>();
 
+        for(FlightSearchInterface searchInterface : flightSearchImplementation)
+        {
+            Callable<List<FlightData>> callable = () -> searchInterface.getFlightSearchData(source,destination);
+            callableList.add((callable));
+        }
 
         try {
-            List<FlightData> indigoResponse = indigoFuture.get();
-            List<FlightData> goResponse = goFuture.get();
-
-            response.addAll(indigoResponse);
-            response.addAll(goResponse);
+            List<Future<List<FlightData>>> futureList = executorService.invokeAll(callableList);
+            for(Future<List<FlightData>> future : futureList)
+            {
+                try {
+                    response.addAll(future.get());
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
             throw new RuntimeException(e);
         }
 
